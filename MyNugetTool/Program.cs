9e3ae63f59af29d001c46151e21415242c9905bd
Shuai <bfshm@qq.com>
2021-02-25 13:57:52 +0800

    init

---
+++ MyNugetTool/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace MyNugetTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Environment.CurrentDirectory;
+            Console.WriteLine($"[MyNugetTool]:{path}");
+            foreach (var file in Directory.EnumerateFiles(path))
+            {
+                Console.WriteLine($"[MyNugetTool]:{file}");
+
+            }
+        }
+    }
+}
